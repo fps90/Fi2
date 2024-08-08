932b31e96a738ddfa77f0e0281dd7a539c4a6ea5
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-08-08 20:59:29 +0300

    Update hastag.py

--- plugins/hastag.py
+++ plugins/hastag.py
@@ -1,23 +1,27 @@
+from MukeshAPI import api
 from pyrogram import filters
+from strings.filters import command
 from YukkiMusic import app
-from TheApi import api
 
 
-@app.on_message(filters.command("hastag"))
+@app.on_message(command("هاشتاكات"))
 async def hastag(bot, message):
 
     try:
         text = message.text.split(" ", 1)[1]
-        res = api.gen_hashtag(text)
+        res = api.hashtag(text)
+        results = " ".join(res)
+        hashtags = results.replace(",", "").replace("[", "").replace("]", "")
+
     except IndexError:
-        return await message.reply_text("Example:\n\n/hastag python")
+        return await message.reply_text("- اكتب هاشتاكات واسم هاشتاك برمجي كمثال بايثون")
 
-    await message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ  ʜᴀsᴛᴀɢ :\n<pre>{res}</pre>", quote=True)
+    await message.reply_text(
+        f"- اليك الهاشتاكات :\n<pre>{hashtags}</pre>", quote=True
+    )
 
 
-__MODULE__ = "Hᴀsʜᴛᴀɢ"
-__HELP__ = """
-**ʜᴀsʜᴛᴀɢ ɢᴇɴᴇʀᴀᴛᴏʀ:**
-
-• `/hashtag [text]`: Gᴇɴᴇʀᴀᴛᴇ ʜᴀsʜᴛᴀɢs ғᴏʀ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ.
-"""
+help = """
+Yᴏᴜ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ʜᴀsʜᴛᴀɢ ɢᴇɴᴇʀᴀᴛᴏʀ ᴡʜɪᴄʜ ᴡɪʟʟ ɢɪᴠᴇ ʏᴏᴜ ᴛʜᴇ ᴛᴏᴘ 𝟹𝟶 ᴀɴᴅ ᴍᴏʀᴇ ʜᴀsʜᴛᴀɢs ʙᴀsᴇᴅ ᴏғғ ᴏғ ᴏɴᴇ ᴋᴇʏᴡᴏʀᴅ sᴇʟᴇᴄᴛɪᴏɴ.
+° /hastag enter word to generate hastag.
+°Exᴀᴍᴘʟᴇ:  /hastag python """
